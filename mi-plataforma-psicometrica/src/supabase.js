e0eb076bbc41ebc00e9011e0115d2afef7d809d5
devspace-rz <diegoaramirezm4@gmail.com>
2026-08-08 15:46:15 -0600

    Update supabase.js

--- mi-plataforma-psicometrica/src/supabase.js
+++ mi-plataforma-psicometrica/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'TUS_DATOS_DE_SUPABASE_URL'
-const SUPABASE_ANON_KEY = 'TUS_DATOS_DE_SUPABASE_ANON_KEY'
+const SUPABASE_URL = 'https://fvdaqwyludcgclaktjtr.supabase.co/rest/v1/'
+const SUPABASE_ANON_KEY = 'sb_publishable_Akn2W_RpStKe1GBh_HEtqw_Z7eDBd9g'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
